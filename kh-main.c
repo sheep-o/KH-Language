3303b3d6840468a3eb304977ca169b8b781f04d6
sheepo <47284480+sheep-o@users.noreply.github.com>
2019-09-24 22:16:27 -0400

    Update kh-main.c

--- kh-main.c
+++ kh-main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
+#include "./kh-std/strings.h"
+
+int index_of(const char str[], const char char_to_find);
+bool starts_with(const char str_input[], const char starts_with[]);
+
+int main(int argc, char* argv[]) 
+{
+    char* input_path = argv[1];
+
+    if (argc < 1) { printf("Error: No input file.\n"); return -1; }
+
+    return 0;
+}
